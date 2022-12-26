b413234e0f57ae29a49e41c7ed349bd5e94abb0d
itstasneem <102755155+itstasneem@users.noreply.github.com>
2022-12-27 03:06:11 +0530

    Data Import and Module Wrapper Function

---
+++ data_import_from_another_js_file/common_js_module/receiver.js
@@ -0,0 +1,4 @@
+
+const xyz = require("./sender");
+
+console.log("Hello World", xyz);
---
+++ data_import_from_another_js_file/common_js_module/sender.js
@@ -0,0 +1,18 @@
+
+// module wrapper function automatically provided by nodejs, no need to write it, 
+
+// ( function (exports, require, module, __filename, __dirname) {
+
+obj = {
+    firstName: "Anwer",
+    lastName: "Sayeed",
+    age: 26,
+    isSingle: true
+}
+
+module.exports = obj;
+
+// })
+
+// you can also console log these variables
+// console.log( exports, require, module, __filename, __dirname );
